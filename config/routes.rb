cdcff20227dd958f5e50ff1a46b5a48b17bd8e5c
Patrick Brown <patrick.arthur.brown@gmail.com>
2013-06-17 17:47:20 -0400

    added mockups implementing twitter bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Beaker::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/search_results"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
