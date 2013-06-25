b8901c5fa6a4c32c041abbcafb05a0a80268a6c3
Patrick Brown <patrick.arthur.brown@gmail.com>
2013-06-25 00:54:56 -0400

    changed to create_table

--- db/migrate/20130624053230_create_components.rb
+++ db/migrate/20130624053230_create_components.rb
@@ -1,8 +1,11 @@
 class CreateComponents < ActiveRecord::Migration
   def change
-    change_table :components do |t|
+    create_table :components do |t|
+      t.string  :part_no
+      t.integer :quantity
+      t.string  :description
       t.decimal :price
-      t.string :supplier
+      t.string  :supplier
       t.integer :rating
 
     end
