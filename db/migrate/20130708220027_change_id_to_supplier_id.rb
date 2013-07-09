a77aef87ef2eab7cf2ea58adcac44a58ed742ed5
Jacob <Jacob@P>
2013-07-08 23:23:45 -0500

    Removed excess files

--- db/migrate/20130708220027_change_id_to_supplier_id.rb
+++
@@ -1,8 +0,0 @@
-class ChangeIdToSupplierId < ActiveRecord::Migration
-  def up
-  	add_column :components, :supplier_id, :integer
-  end
-
-  def down
-  end
-end
--- db/migrate/20130708223115_add_index_to_components_supplier_id.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToComponentsSupplierId < ActiveRecord::Migration
-  def change
-  	add_index :components, :supplier_id, unique: true
-  end
-end
