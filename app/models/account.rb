a7ab88e7dae8624a4a11a4b9fd0d70162c212b98
Jacob <Jacob@P>
2013-07-17 12:43:39 -0500

    Removed Supplier & Accounts related files

--- app/models/account.rb
+++
@@ -1,6 +0,0 @@
-class Account < ActiveRecord::Base
-  attr_accessible :name
-  # belongs_to :user
-  # belongs_to :supplier
-
-end
--- app/models/supplier.rb
+++
@@ -1,6 +0,0 @@
-class Supplier < ActiveRecord::Base
-	attr_accessible :name, :overall_rating, :description
-	belongs_to :user
-	belongs_to :component
-
-end
