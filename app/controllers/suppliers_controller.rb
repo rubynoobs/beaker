b461bf9d66146a8fccc93744bcd40f236d1fa56f
Jacob <Jacob@P>
2013-07-30 13:04:22 -0500

    Added dynamic properties to individual supplier page

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -3,6 +3,10 @@ class SuppliersController < ApplicationController
 		@suppliers = User.where(supplier: true).paginate(page: params[:page]).sort_by!{ |m| m.name.downcase } # sorted alphabetically
 	end
 
+	def show
+		@supplier = User.find(params[:id])
+	end
+
 	# private
 	# 	def supplier_params
 	# 		params.require(:component).permit(:description, :part_no, :price, :quantity, :rating)
